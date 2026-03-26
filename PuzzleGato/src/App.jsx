0009827734c507958769c2e2f791d74b15e87e50
Andres-Soler <andres.solers@uniagustiniana.edu.co>
2026-03-26 23:58:28 +0000

    GatoPuzzle

---
+++ PuzzleGato/src/App.jsx
@@ -0,0 +1,12 @@
+import PuzzleGato from "./PuzzleGato";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <PuzzleGato />
+    </div>
+  );
+}
+
+export default App;
---
+++ PuzzleGato/src/PuzzleGato.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import "./PuzzleGato.css";
+
+const columns = 8;
+const image = "/gato.jpg";
+
+export default function PuzzleGato() {
+
+  const [pieces, setPieces] = useState([]);
+
+  useEffect(() => {
+    const arr = [...Array(columns).keys()];
+    setPieces(arr.sort(() => Math.random() - 0.5));
+  }, []);
+
+  const solved =
+  pieces.length === columns &&
+  pieces.every((piece, index) => piece === index)
+
+  function swap(i, j) {
+    const newPieces = [...pieces];
+    [newPieces[i], newPieces[j]] = [newPieces[j], newPieces[i]];
+    setPieces(newPieces);
+  }
+
+    return (
+        <div className="puzzle-container">
+
+            {/* fondos decorativos */}
+            <div className="background-shape shape1"></div>
+            <div className="background-shape shape2"></div>
+
+            <div className="game-card">
+            <h2>Puzzle Gato 🐱</h2>
+
+            <div className={`puzzle-row ${solved ? "solved" : ""}`}>
+                {pieces.map((piece, index) => (
+                <div
+                    key={index}
+                    className="puzzle-piece"
+                    onClick={() => swap(index, (index + 1) % columns)}
+                    style={{
+                    width: `${100 / columns}%`,
+                    backgroundImage: `url(${image})`,
+                    backgroundSize: `${columns * 100}% 100%`,
+                    backgroundPosition: `${(piece * 100) / (columns - 1)}% 0`
+                    }}
+                />
+                ))}
+            </div>
+
+            {solved && <h3>🎉 ¡Lo resolviste!</h3>}
+            </div>
+
+        </div>
+    );
+}
